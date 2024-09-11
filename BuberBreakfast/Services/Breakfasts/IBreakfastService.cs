a0c1f7e29136416e58a9b31daa28456dd1a1e8e7
Guide <gfari@littlefishapp.com>
2024-09-11 12:53:58 +0200

    wip: breakfast service

---
+++ BuberBreakfast/Services/Breakfasts/IBreakfastService.cs
@@ -0,0 +1,11 @@
+using BuberBreakfast.Models;
+
+namespace BuberBreakfast.Services.Breakfasts;
+
+public interface IBreakfastService
+{
+    void CreateBreakfast(Breakfast breakfast);
+    Breakfast GetBreakfast(Guid id);
+    // BreakfastResponse UpsertBreakfast(Guid id, UpsertBreakfastRequest request);
+    // BreakfastResponse DeleteBreakfast(Guid id);
+}
