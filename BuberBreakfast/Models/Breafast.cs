606f076f8f6ea1e048e91c72355d2299937f0be7
Guide <gfari@littlefishapp.com>
2024-09-11 23:28:15 +0200

    wip: model update. preps for improved error handling

--- BuberBreakfast/Models/Breafast.cs
+++ BuberBreakfast/Models/Breafast.cs
@@ -1,3 +1,6 @@
+using ErrorOr;
+using BuberBreakfast.Contracts.Breakfast;
+
 namespace BuberBreakfast.Models;
 
 public class Breakfast
@@ -29,5 +32,65 @@ public class Breakfast
         LastModifiedDateTime = lastModifiedDateTime;
         Savory = savory;
         Sweet = sweet;
+    }
+
+    public static ErrorOr<Breakfast> Create(
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime,
+        List<string> savory,
+        List<string> sweet,
+        Guid? id = null)
+    {
+        List<Error> errors = new();
+
+        if (name.Length is < MinNameLength or > MaxNameLength)
+        {
+            errors.Add(Errors.Breakfast.InvalidName);
+        }
+
+        if (description.Length is < MinDescriptionLength or > MaxDescriptionLength)
+        {
+            errors.Add(Errors.Breakfast.InvalidDescription);
+        }
+
+        if (errors.Count > 0)
+        {
+            return errors;
+        }
+
+        return new Breakfast(
+            id ?? Guid.NewGuid(),
+            name,
+            description,
+            startDateTime,
+            endDateTime,
+            DateTime.UtcNow,
+            savory,
+            sweet);
+    }
+
+    public static ErrorOr<Breakfast> From(CreateBreakfastRequest request)
+    {
+        return Create(
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            request.Savory,
+            request.Sweet);
+    }
+
+    public static ErrorOr<Breakfast> From(Guid id, UpsertBreakfastRequest request)
+    {
+        return Create(
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            request.Savory,
+            request.Sweet,
+            id);
     }
 }
